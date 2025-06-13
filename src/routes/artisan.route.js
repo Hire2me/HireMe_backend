5e67f0f38691ee111500220bc2de3015894ba4eb
TimmyBigi <abimbolaayomide223@gmail.com>
2025-06-13 11:08:39 +0100

    fixed

--- src/routes/artisan.route.js
+++ src/routes/artisan.route.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Artisan = require('../models/artisan.model');
 const artisanController = require('../controllers/artisan.controller');
+const { authenticateToken } = require('../middleware/auth');
 
 
 router.post('/signup', artisanController.signup);
 router.post('/login', artisanController.login);
 router.post('/forgot-password', artisanController.forgotPassword);
 router.post('/reset-password', artisanController.resetPassword);
-router.post('/verify-email', artisanController.verifyEmail);
+router.post('/verify-email', authenticateToken, artisanController.verifyEmail);
 
 router.get('/debug/:email', async (req, res) => {
     try {
