9c622d540366a3d5db51d52544a9fdac669610df
Hire2me <ffindartisans@gmail.com>
2025-06-30 08:09:12 -0700

    Update upload.routes.js

--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
 const { uploadImages } = require('../controllers/upload.controller');
+const { authenticateToken } = require('../middleware/auth');
+const  upload = require ('../image/multer');
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-router.post('/:userId', upload.array('photos', 10), uploadImages);
+router.post('/:userId', upload.array('photos', 10), authenticateToken, uploadImages);
 
 module.exports = router;
