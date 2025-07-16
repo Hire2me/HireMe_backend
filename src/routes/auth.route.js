e59796502fe8e47a0abbfc784d633cebedaf5c6b
Hire2me <ffindartisans@gmail.com>
2025-07-16 04:09:44 -0700

    Update auth.route.js

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
-// const { googleMobileLogin } = require('../controller/google.controller');
+
 
 const router = express.Router();
 
@@ -27,19 +27,12 @@ router.get(
     failureRedirect: "/login",
   }),
   (req, res) => {
-    // Optional: Issue a JWT here for APIs/mobile apps
-    // const token = jwt.sign(
-    //     { id: req.user.id, email: req.user.email },
-    //     process.env.JWT_SECRET,
-    //     { expiresIn: process.env.JWT_EXPIRES_IN }
-    // );
-    // res.json({ token, message: 'Google login successful' });
+    
 
     res.redirect("/dashbroad"); // fixed typo from 'dashbroad'
   }
 );
 
-// Mobile login with Google token from client
-// router.post('/google/mobile', googleMobileLogin);
+
 
 module.exports = router;
